9781cd7eff25ea599335d2994245f3d863cd60ff
unknown <nimbusabolt@gmail.com>
2024-09-18 09:53:54 -0600

    experience updated

--- utils/data/experience.js
+++ utils/data/experience.js
@@ -1,20 +1,26 @@
 export const experiences = [
   {
     id: 1,
-    title: "Senior AI and Full Stack Developer",
-    company: "Freelance",
-    duration: "(Jul 2022 - Present)",
+    title: "AI and Full Stack Developer",
+    company: "Freelancer",
+    duration: "(Jan 2024 - Present)",
   },
   {
     id: 2,
-    title: "LLM Developer",
-    company: "Vicarious",
-    duration: "(May 2019 - Jul 2022)",
+    title: "Lead Developer",
+    company: "SoftWorks Studios",
+    duration: "(Jul 2023 - Jan 2024)",
   },
   {
     id: 3,
-    title: "Junior AI Developer",
-    company: "Vicarious",
-    duration: "(Apr-2017 - May2019)",
+    title: "Full Stack Developer",
+    company: "SoftWorks Studios",
+    duration: "(May 2018 - Jul 2023)",
+  },
+  {
+    id: 4,
+    title: "Software Developer Intern",
+    company: "SoftWorks Studios",
+    duration: "(Apr 2017 - May 2018)",
   },
 ];
